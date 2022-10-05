58a116960b031c1e7ecdec8e0a726af9a7c0177d
wighawag <wighawag@gmail.com>
2022-10-05 11:20:37 +0100

    better UI

--- examples/web-demo/src/App.svelte
+++ examples/web-demo/src/App.svelte
@@ -2,7 +2,11 @@
 	import {browserIndexer, state, stringify} from '$lib/state/State';
 </script>
 
-{$browserIndexer?.syncPercentage}
-<p>
+<progress value={($browserIndexer?.syncPercentage || 0) / 100} style="width:100%;" />
+
+<p>block processed: {$browserIndexer?.numBlocksProcessedSoFar.toLocaleString()}</p>
+<p>num events: {$browserIndexer?.nextStreamID.toLocaleString()}</p>
+
+<details>
 	{$state ? stringify($state) : $browserIndexer ? stringify($browserIndexer) : 'Please wait...'}
-</p>
+</details>
