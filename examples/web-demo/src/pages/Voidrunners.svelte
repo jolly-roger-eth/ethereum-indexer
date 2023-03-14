f2a0da34727b3fc98a4dd9f500ebb80dcd7cebfe
wighawag <wighawag@gmail.com>
2023-03-14 09:40:30 +0000

    can use state without conditional

--- examples/web-demo/src/pages/Voidrunners.svelte
+++ examples/web-demo/src/pages/Voidrunners.svelte
@@ -3,16 +3,12 @@
 	import {createIndexeInitializer} from '../lib/blockchain/indexer';
 	import IndexerButton from '../lib/components/IndexerButton.svelte';
 	import IndexerProgress from '../lib/components/IndexerProgress.svelte';
-	import IndexerStatus from '../lib/components/IndexerStatus.svelte';
-	import StateValues from '../lib/components/StateValues.svelte';
-	const {status, state, syncing, initialize} = createIndexeInitializer(
-		processor,
-		contractsData,
-		contractsData[0].chainId
-	);
+	const {state, syncing, initialize} = createIndexeInitializer(processor, contractsData, contractsData[0].chainId);
 </script>
 
 <IndexerButton {initialize} chainId={contractsData[0].chainId} accountsToUse={false} />
 <IndexerProgress {syncing} />
-<IndexerStatus {status} {syncing} />
-<StateValues {state} {syncing} />
+
+{#each $state.spaceships as spaceship}
+	<p>{spaceship.tokenID}</p>
+{/each}
