e6caf3446b1597ecafd4adb820b4d5553f8612ca
wighawag <wighawag@gmail.com>
2023-03-20 11:23:24 +0000

    remove ununsed ethereum-indexer-tester

--- packages/ethereum-indexer-tester/cli.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env ts-node
-import 'dotenv/config';
-import 'named-logs-console'; // active named-logs and use console as basic logger
-
-const oclif = require('@oclif/core');
-
-oclif.run().then(require('@oclif/core/flush')).catch(require('@oclif/core/handle'));
