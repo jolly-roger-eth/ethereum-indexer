c7399fde8c0abfc637aa05c387be6c986d7e54ec
wighawag <wighawag@gmail.com>
2023-03-22 12:27:48 +0000

    cleanup

--- packages/ethereum-indexer/src/utils/promises.ts
+++ packages/ethereum-indexer/src/utils/promises.ts
@@ -1,37 +1,11 @@
-// TODO make it a library
-
 type ResolveFunction<T> = (value: T | PromiseLike<T>) => void;
 type RejectFunction = (reason?: any) => void;
-
-// export type RejectablePromise<T> = PromiseLike<T> & {
-// 	reject(err: any): void;
-// 	rejected: boolean;
-// };
-
-// export class RejectablePromiseAlreadyRejectedError extends Error {
-// 	constructor() {
-// 		super('RejectablePromiseAlreadyRejectedError');
-// 	}
-// }
-
-// export class RejectablePromiseRejected extends Error {
-// 	constructor() {
-// 		super('RejectablePromiseRejected');
-// 	}
-// }
 
 export type CancellablePromise<T> = PromiseLike<T> & {
 	cancel(): void;
 	cancelled: boolean;
-	// cancellable: boolean;
 	completed: boolean;
 };
-
-// export class CancellablePromiseError extends Error {
-// 	constructor() {
-// 		super('CancellablePromiseError');
-// 	}
-// }
 
 export class CancellablePromiseCancelled extends Error {
 	constructor() {
@@ -39,48 +13,16 @@ export class CancellablePromiseCancelled extends Error {
 	}
 }
 
-// export function createRejectablePromise<T>(
-// 	executor: (resolve: ResolveFunction<T>, reject: RejectFunction) => void
-// ): RejectablePromise<T> {
-// 	const promise = new Promise<T>((resolve, reject) => {
-// 		let rejected = false;
-// 		function rejectIfNotRejectedAlready(err: any) {
-// 			if (!rejected) {
-// 				rejected = true;
-// 				reject(err);
-// 			}
-// 		}
-// 		function resolveIfNotRejectedAlready(result: T | PromiseLike<T>) {
-// 			if (!rejected) {
-// 				resolve(result);
-// 			}
-// 		}
-
-// 		(promise as any).reject = () => {
-// 			if (rejected) {
-// 				throw new RejectablePromiseAlreadyRejectedError();
-// 			} else {
-// 				rejected = true;
-// 				reject(new RejectablePromiseRejected());
-// 			}
-// 		};
-// 		executor(resolveIfNotRejectedAlready, rejectIfNotRejectedAlready);
-
-// 		Object.defineProperty(promise, 'rejected', {
-// 			get: function () {
-// 				return rejected;
-// 			},
-// 		});
-// 	});
-
-// 	return promise as PromiseLike<T> as RejectablePromise<T>;
-// }
-
-// type OnePromiseArgFn<T> = T extends void ? () => void : (arg: Promise<T>) => void;
-// type UnlessCancelledFunction = <U>(...a: Parameters<OnePromiseArgFn<U>>) => U extends undefined ? void : Promise<U>;
-
 export type UnlessCancelledFunction = <T>(arg: Promise<T>) => Promise<T>;
 
+/**
+ *
+ * @param executor function that receive the usual resolve and reject function
+ * plus a unlessCancelled function that reject if the promise is already cancelled
+ * and a cancel function that can be called to cancel the promise
+ * @param onCancel an optional callback that is called when the promise is cancelled
+ * @returns a promise with a cancel function along with 2 status getter `completed` and `cancelled`
+ */
 export function createCancellablePromise<T>(
 	executor: (
 		resolve: ResolveFunction<T>,
@@ -93,31 +35,24 @@ export function createCancellablePromise<T>(
 	let resolve: ResolveFunction<T>;
 	let reject: RejectFunction;
 	let cancelled = false;
-	// let cancellable = true;
 	let completed = false;
 
 	function rejectIfNotCancelledAlready(err: any) {
 		if (!cancelled && !completed) {
-			// cancellable = false;
 			completed = true;
 			reject(err);
 		}
 	}
 	function resolveIfNotCancelledAlready(result: T | PromiseLike<T>) {
 		if (!cancelled && !completed) {
-			// cancellable = false;
 			completed = true;
 			resolve(result);
 		}
 	}
 
 	function cancel() {
-		// if (!cancellable) {
-		// 	throw new CancellablePromiseError();
-		// }
 		if (!cancelled) {
 			cancelled = true;
-			// cancellable = false;
 			reject(new CancellablePromiseCancelled());
 			if (onCancel) {
 				onCancel();
@@ -125,11 +60,6 @@ export function createCancellablePromise<T>(
 		}
 	}
 
-	// function rejectIfCancelled(): void {
-	// 	if (cancelled) {
-	// 		throw new CancellablePromiseCancelled();
-	// 	}
-	// }
 	function unlessCancelled<U>(p: Promise<U>): Promise<U> {
 		return p.then((v) => {
 			if (cancelled) {
@@ -157,11 +87,6 @@ export function createCancellablePromise<T>(
 			return cancelled;
 		},
 	});
-	// Object.defineProperty(promise, 'cancellable', {
-	// 	get: function () {
-	// 		return cancellable;
-	// 	},
-	// });
 	Object.defineProperty(promise, 'completed', {
 		get: function () {
 			return completed;
@@ -183,6 +108,9 @@ export type ActionOperations<T> = CancelOperations & {
 	reject: RejectFunction;
 };
 
+/**
+ * An action is a special object that manage an underlying promise to ensure it is executed in a proper manner
+ */
 export function createAction<T, U = undefined, C = undefined>(
 	execute: U extends undefined
 		? (action: ActionOperations<T>) => void | Promise<T>
@@ -286,152 +214,3 @@ export function createAction<T, U = undefined, C = undefined>(
 		getContext(): C | undefined;
 	};
 }
-
-// // TEST
-// const action = createPromiseAction<number>(({resolve, unlessCancelled}) => {
-// 	console.log('hello');
-// 	resolve(1);
-// });
-// action.once();
-
-// const action2 = createPromiseAction<number, string>(({resolve, unlessCancelled}, args) => {
-// 	resolve(1);
-// 	console.log(args);
-// });
-// action2.once('hello');
-
-// export function createARunOnceAtATime<T>() {
-// 	let _promise: CancellablePromise<T> | undefined;
-
-// 	return {
-// 		promise(
-// 			execute: (
-// 				resolve: ResolveFunction<T>,
-// 				reject: RejectFunction,
-// 				unlessCancelled: <U>(p: Promise<U>) => Promise<U>,
-// 				cancel: () => void
-// 			) => void,
-// 			mode: 'queue' | 'wait' | 'force' | 'once' = 'wait'
-// 		) {
-// 			if (_promise) {
-// 				if (_promise.completed) {
-// 					if (mode === 'once') {
-// 						return _promise;
-// 					}
-// 				} else if (!_promise.cancelled) {
-// 					if (mode === 'force') {
-// 						_promise.cancel();
-// 					} else if (mode === 'queue') {
-// 						const p = _promise;
-// 						_promise = _promise.then(() => {
-// 							return createCancellablePromise(
-// 								(resolve, reject, unlessCancelled, cancel) => {
-// 									execute(resolve, reject, unlessCancelled, cancel);
-// 								},
-// 								() => {
-// 									p.cancel();
-// 								}
-// 							);
-// 						}) as CancellablePromise<T>;
-// 					} else {
-// 						return _promise;
-// 					}
-// 				}
-// 			}
-// 			_promise = createCancellablePromise((resolve, reject, unlessCancelled, cancel) => {
-// 				execute(resolve, reject, unlessCancelled, cancel);
-// 			});
-// 			return _promise;
-// 		},
-// 	};
-// }
-
-// ------------------------------------------------------------------------------------------------
-// EXPERIMENTAL HELPERS
-// ------------------------------------------------------------------------------------------------
-// export function createManageablePromise<
-// 	U = unknown,
-// 	T extends PromiseLike<U> = PromiseLike<U>,
-// 	P extends [ResolveFunction<U>, RejectFunction, ...any[]] = [resolve: ResolveFunction<U>, reject: RejectFunction]
-// >(factory: (executor: (...p: P) => void) => T, callbacks?: {onResolve: ResolveFunction<U>; onReject: RejectFunction}) {
-// 	let _promise: T | undefined;
-// 	let _resolve: ResolveFunction<U> | undefined;
-// 	let _reject: RejectFunction | undefined;
-// 	function clear(): {reject: RejectFunction; resolve: ResolveFunction<U>} | undefined {
-// 		if (_promise) {
-// 			const past = {reject: _reject as RejectFunction, resolve: _resolve as ResolveFunction<U>};
-// 			_promise = undefined;
-// 			_resolve = undefined;
-// 			_reject = undefined;
-// 			return past;
-// 		}
-// 		return undefined;
-// 	}
-// 	return {
-// 		promise(execute?: (...args: P) => void) {
-// 			if (_promise) {
-// 				return _promise;
-// 			}
-// 			_promise = factory((resolve, reject, ...args) => {
-// 				_resolve = resolve;
-// 				_reject = reject;
-// 				if (execute) {
-// 					(execute as any)(...args);
-// 				}
-// 			});
-// 			return _promise;
-// 		},
-// 		reject(err: unknown) {
-// 			if (_reject) {
-// 				clear()?.reject(err);
-// 				callbacks?.onReject(err);
-// 			}
-// 		},
-// 		resolve(value: U) {
-// 			if (_resolve) {
-// 				clear()?.resolve(value);
-// 				callbacks?.onResolve(value);
-// 			}
-// 		},
-// 	};
-// }
-
-// this help with types
-// TODO can typescript handle it
-// export function createManageableCancellablePromise<T>() {
-// 	return createManageablePromise<
-// 		T,
-// 		CancellablePromise<T>,
-// 		Parameters<Parameters<typeof createCancellablePromise<T>>[0]>
-// 	>(createCancellablePromise);
-// }
-
-// // type TTT = CancellablePromise<
-// const test = createManageablePromise<
-// 	string,
-// 	CancellablePromise<string>,
-// 	Parameters<Parameters<typeof createCancellablePromise<string>>[0]>
-// >(createCancellablePromise);
-
-// export function testPromise(seconds: number): Promise<string> {
-// 	return new Promise((resolve) => setTimeout(() => resolve('hello'), seconds * 1000));
-// }
-
-// test.promise(async (resolve, reject, unlessCancelled) => {
-// 	const t = await unlessCancelled(testPromise(2));
-// 	console.log(t);
-// 	resolve('dsds');
-// });
-
-// const p = createCancellablePromise(async (resolve, reject, unlessCancelled) => {
-// 	const t = await unlessCancelled(testPromise(2));
-// 	console.log(t);
-// 	resolve('dsds');
-// });
-
-// const test2 = createManageableCancellablePromise();
-// test2.promise(async (resolve, reject, unlessCancelled) => {
-// 	const t = await unlessCancelled(testPromise(2));
-// 	console.log(t);
-// 	resolve('dsds');
-// });
