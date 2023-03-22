a88a77f36c47c960a3060d24011b6d8c8a89ad0f
wighawag <wighawag@gmail.com>
2023-03-22 15:37:40 +0000

    fix

--- packages/ethereum-indexer-server/src/pages/index.ts
+++ packages/ethereum-indexer-server/src/pages/index.ts
@@ -4,7 +4,7 @@ import fs from 'fs';
 import {dirname} from 'path';
 import {fileURLToPath} from 'url';
 
-const _dirname = dirname(fileURLToPath(import.meta.url));
+const _dirname = typeof __dirname !== 'undefined' ? __dirname : dirname(fileURLToPath(import.meta.url));
 
 function page(name: string): string {
 	return fs.readFileSync(path.join(_dirname, '../templates', name), 'utf-8');
--- packages/ethereum-indexer-server/tsup.config.ts
+++ packages/ethereum-indexer-server/tsup.config.ts
@@ -9,11 +9,4 @@ export default defineConfig({
 	async onSuccess() {
 		fs.copySync('templates/', 'dist/');
 	},
-
-	// from https://github.com/evanw/esbuild/issues/1492#issuecomment-893144483
-	// to allow the use of import.meta.url in cjs
-	inject: ['./import-meta-url.js'],
-	define: {
-		'import.meta.url': 'import_meta_url',
-	},
 });
